9a027487e09fa5a1bcf08d374acadba6a901b276
Alex Di3ell <a.n.degilevich@gmail.com>
2019-05-10 14:08:45 +0300

    add_css

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,21 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
-import axios from 'axios';
-import SuperComponent from '../components/SuperComponent';
+
+import { Button, Container } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-
-class Index extends SuperComponent {
+class Index extends React.Component {
    
-        static async getInitialProps() {
-            let userData = {};
-             try {
-                const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-                 userData = response.data;
-                }   catch(err){
-                    console.error(err);
-                }       
-            return { initialData: [1,2,3,4], userData};
-        }
-
-    constructor(props) {
-   
-        super(props);
-
-
-        this.state = {
-            title : 'I am index page'
-        }
-
-        this.updateTitle = this.updateTitle.bind(this)
-    }
-
-    componentDidMount() {
-        console.log('componentDidMount');
-
-    }
-
-    componentDidUpdate() {
-        console.log('componentDidUpdate');
-    }
-    componentWillMount() {
-        console.log('componentWillMount');
-    }
-
-    updateTitle() {
-        this.setState({title: 'I am Update Page'});
-    }
-
     render() {
 
-        const { title } = this.state;
-        const {userData, initialData } = this.props;
         return(
                 <BaseLayout>
-
-                    <h1 className='fromPage'> I'm index page from Class component, and i was updated!!!</h1>
-                    <h2> { title } </h2>
-                    <h2> { userData.title } </h2>
-                    <button onClick={this.updateTitle}>Update Page</button>
+                    <Container>
+                        <Button color="danger">Danger!</Button> 
+                    </Container>
                 </BaseLayout>                           
         )
     }
@@ -65,4 +23,5 @@ class Index extends SuperComponent {
 
 
 export default Index;
+
 
