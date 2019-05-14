3c66bb8f1f0fe848060a97a3dc299db707f843cf
alexdi3ell <a.n.degilevich@gmail.com>
2019-05-14 03:04:13 +0300

    work with cookies

--- services/auth0.js
+++ services/auth0.js
@@ -1,23 +1,24 @@
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
 
- class Auth0 {
+    class Auth0 {
 
-     constructor() {
-        this.auth0 = new auth0.WebAuth({
-            domain: 'dev-ie1nel7a.auth0.com',
-            clientID: 'h40rOp1ImpP0Lirb4usseT01aXD1JQ0H',
-            redirectUri: 'http://localhost:3000/callback',
-            responseType: 'token id_token',
-            scope: 'openid'
-        });
+        constructor() {
+            this.auth0 = new auth0.WebAuth({
+                domain: 'dev-ie1nel7a.auth0.com',
+                clientID: 'h40rOp1ImpP0Lirb4usseT01aXD1JQ0H',
+                redirectUri: 'http://localhost:3000/callback',
+                responseType: 'token id_token',
+                scope: 'openid'
+            });
 
         this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
         this.handleAuthentication = this.handleAuthentication.bind(this);
-
+        this.isAuthenticated = this.isAuthenticated.bind(this);
         }
 
         handleAuthentication() {
-            debugger;
             return new Promise((resolve, reject) => {
                 this.auth0.parseHash((err, authResult) => {
                     if (authResult && authResult.accessToken && authResult.idToken) {
@@ -31,14 +32,39 @@ import auth0 from 'auth0-js';
             })
         }
     
-    setSession() {
-        //Save tokens
-    }
+        setSession(authResult) {
+            debugger;
+            // Set the time that the Access Token will expire at
+            const expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
 
-  login() {
-    this.auth0.authorize();
-  }
-}
+            Cookies.set('user', authResult.idTokenPayload);
+            Cookies.set('jwt', authResult.idToken);
+            Cookies.set('expiresAt', authResult.expiresAt);
+          }
+
+        logout() {
+            Cookies.remove('user');
+            Cookies.remove('jwt');
+            Cookies.remove('expiresAt');
+
+            this.auth0.logout({
+            returnTo:'',
+            clientId: 'h40rOp1ImpP0Lirb4usseT01aXD1JQ0H'
+            })
+        }
+
+        login() {
+            this.auth0.authorize();
+        }
+
+
+        isAuthenticated() {
+            // Check whether the current time is past the
+            // access token's expiry time
+            let expiresAt = Cookies.getJSON('expiresAt');
+            return new Date().getTime() < expiresAt;
+        }
+    }
 
 const auth0Client = new Auth0();
 
